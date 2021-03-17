fdb6afa83f4f9d7ee9cf2122a436a08c075218cf
dorji khandu <khandudorji47@gmail.com>
2021-03-17 21:06:18 +0545

    user pages setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_091133) do
+ActiveRecord::Schema.define(version: 2021_03_17_143045) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(version: 2021_03_17_091133) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["distype_id"], name: "index_products_on_distype_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "products", "categories"
